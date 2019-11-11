7a6473512b44b86cfa53c47e0abe07e384bb86d7
Louis Alan <louis@tinkerlust.com>
2019-11-11 16:07:01 +0700

    adding navigation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+
 const Index = () => (
   <div>
     <p>Hello, Next!</p>
+    <Link href="/about">
+      <a title="about page">About Page</a>
+    </Link>
   </div>
 );
 
